09cca82505e0e581912a9ace638a75af39c8ee98
Rajnish Kumar <RajnishKMehta@proton.me>
2026-06-22 11:09:44 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 val appId = "io.github.rajnishkmehta.dhwanicontrol"
 
 // VERSIONING
-val appVersionName = "1.0.1"
-val appVersionCode = 2
+val appVersionName = "1.0.2"
+val appVersionCode = 3
 
 // Extract versions from appVersionName
 val versionParts = appVersionName.split(".")
