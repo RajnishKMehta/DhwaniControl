cf2be54b3f588f36feff5bc0e8a1010c7ad7f562
Rajnish Kumar <RajnishKMehta@proton.me>
2026-07-08 01:30:05 +0530

    Update app version and signing configurations
    Updated app version to 1.1.1 and version code to 5. Adjusted signing configurations and dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 val appId = "io.github.rajnishkmehta.dhwanicontrol"
 
 // VERSIONING
-val appVersionName = "1.1.0"
-val appVersionCode = 4
+val appVersionName = "1.1.1"
+val appVersionCode = 5
 
 // Extract versions from appVersionName
 val versionParts = appVersionName.split(".")
