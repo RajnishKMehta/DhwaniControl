c6a44c9cd827339e26f3feeb0292abf2171d2f8d
Rajnish Kumar <RajnishKMehta@proton.me>
2026-09-11 22:28:19 +0530

    Update app version to 1.2.2 and code to 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 val appId = "io.github.rajnishkmehta.dhwanicontrol"
 
 // VERSIONING
-val appVersionName = "1.2.1"
-val appVersionCode = 7
+val appVersionName = "1.2.2"
+val appVersionCode = 8
 
 // Extract versions from appVersionName
 val versionParts = appVersionName.split(".")
